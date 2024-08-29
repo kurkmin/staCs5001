eb253b655216d3648530222b32ed54b5717d9b7b
Sumin Oh <kurkmin@gmail.com>
2024-08-30 08:17:33 +0900

    comments added

--- practices/tcp/src/ClientSocketExample.java
+++ practices/tcp/src/ClientSocketExample.java
@@ -8,16 +8,22 @@ import java.net.Socket;
 public class ClientSocketExample {
 
     public static void main(String[] args) throws IOException {
+        // create a client socket to connect to a Server on "localhost" running port 8888
         Socket socket = new Socket("localhost", 8888);
 
+        // reader
         InputStreamReader isr = new InputStreamReader(socket.getInputStream());
         BufferedReader in = new BufferedReader(isr);
+        // writer
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
 
+        // send the text to the connected server
         out.println("Ping");
+        // read a responding line from the connected server
         String res = in.readLine();
         System.out.println("Client: message replied from the server: " + res);
 
+        // close the socket connection
         socket.close();
     }
 
--- practices/tcp/src/ServerSocketExample.java
+++ practices/tcp/src/ServerSocketExample.java
@@ -9,18 +9,22 @@ public class ServerSocketExample {
         // the TCP server socket runs/listens on port 8888
         ServerSocket ss = new ServerSocket(8888);
         System.out.println("Server: waiting for the client request...");
-        // waits for an incoming connection from client
+        // waits for an incoming connection request from client
         Socket conn = ss.accept();
 
-
+        // reader
         InputStreamReader isr = new InputStreamReader(conn.getInputStream());
         BufferedReader in = new BufferedReader(isr);
+        // writer
         PrintWriter out = new PrintWriter(conn.getOutputStream(), true);
 
+        // read a line from the connected client
         String req = in.readLine();
         System.out.println("Server: message received from the client: "  + req);
+        // send a responding line to the connected client
         out.println("Pong");
 
+        // close the socket connection
         conn.close();
     }
 }
